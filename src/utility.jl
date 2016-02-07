96d13620bcbdf26bf5e4812a5bd5388ef3ea7d24
Lanfeng Pan <panlanfeng@gmail.com>
2016-02-07 12:52:40 -0600

    eigenvalues in asymptoticdistribution error

--- src/utility.jl
+++ src/utility.jl
@@ -582,7 +582,7 @@ function asymptoticdistribution(X::Matrix{Float64}, Y::AbstractArray{Bool, 1}, g
     I_λ_η = I_all[(J+3*C):(J+5*C-1), (J+3*C):(J+5*C-1)] - I_all[(J+3*C):(J+5*C-1), 1:(J+3*C-1)] * inv(I_all[1:(J+3*C-1), 1:(J+3*C-1)]) * I_all[1:(J+3*C-1),(J+3*C):(J+5*C-1) ]
     debuginfo && println(round(I_λ_η, 6))
     D, V = eig(I_λ_η)
-    D[D.<tol2] = tol2
+    D[D.<0.] = 0.
     I_λ_η2 = V * diagm(sqrt(D)) * V'
     u = randn(nrep, 2*C) * I_λ_η2
     EM = zeros(nrep, C)
