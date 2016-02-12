27272efae5d9d6f47c50d651d045919c4925a91b
Lanfeng Pan <panlanfeng@gmail.com>
2016-02-12 15:39:27 -0600

    the sqr fun for gmm

--- src/arithmetic.jl
+++ src/arithmetic.jl
@@ -123,6 +123,14 @@ end
 # multiply!(x::Float64, y::AbstractArray{Float64}, n::Int64=length(y)) = multiply!(y, x, y, n)
 multiply!(x::AbstractArray{Float64}, y::Float64, n::Int64=length(x)) = multiply!(x, x, y, n)
 
+function sqr!(res::AbstractArray{Float64}, x::AbstractArray{Float64}, n::Int64=length(x))
+   for i in 1:n
+       @inbounds res[i] = x[i]*x[i]
+   end
+   nothing
+end
+sqr!(x::AbstractArray{Float64}, n::Int64=length(x)) = sqr!(x, x, n)
+
 function negate!(res::AbstractArray{Float64}, x::AbstractArray{Float64}, n::Int64=length(x))
    for i in 1:n
        @inbounds res[i] = -x[i]
