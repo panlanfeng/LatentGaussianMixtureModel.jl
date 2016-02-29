e0d38ebc467078507da42a971bf1dea0f5cb1a42
Lanfeng Pan <panlanfeng@gmail.com>
2016-02-28 21:08:03 -0600

    extract datagen as independent file

--- examples/TestAsymptoticDistribution.jl
+++ examples/TestAsymptoticDistribution.jl
@@ -1,46 +1,17 @@
 #find out the test statistic when m0=1
 import LatentGaussianMixtureModel
-import Distributions, StatsBase
-@everywhere using Distributions, StatsBase
-
-
+import Distributions
+include(joinpath(Pkg.dir("LatentGaussianMixtureModel"), "examples/datagen.jl"))
 #Brun calculate the statistic for one data set;
 #b is the the random number seed, from 1 to 100
 @everywhere function Brun(b::Integer, Ctrue::Integer, Calternative::Integer; debuginfo::Bool=false, ntrials::Int=25)
-    nF = 282
+    n = 282
     srand(100)
-    n_ij = round(Int64, rand(Poisson(5), 282).+rand(Exponential(45), 282))
-    N = sum(n_ij)
+    n_ij = round(Int64, Distributions.rand(Distributions.Poisson(5), n).+Distributions.rand(Distributions.Exponential(45), n))
+    N = sum(n_ij); J=2
 
-    groupindex = inverse_rle(1:nF, n_ij)
-    J=2  #42 #beta dimension
-    srand(200*b)
-    X = rand(TDist(8), N, 2) .* [14.71 4.47] ./ std(TDist(8))
-    betas_true=[0.0274, 0.00878]
-    if Ctrue == 1
-        mu_true = [-1.54]
-        wi_true = [1.0]
-        sigmas_true = [0.3493]
-    elseif Ctrue == 2
-        mu_true = [-2.0858,-1.4879]
-        wi_true = [0.0828,0.9172]
-        sigmas_true = [0.6735,0.2931]
-    elseif Ctrue == 3
-        mu_true = [log(1/0.779 - 1) - 4.0, log(1/0.779 - 1) + 1.0, log(1/0.779 - 1) + 4.0;]
-        wi_true = [.3, .4, .3]
-        sigmas_true = [1.2, .8, .9]
-    end
+    X, Y, groupindex, wi_true, mu_true, sigmas_true, m, gamma_true = datagen(n, Ctrue, N, J, n_ij, b=b)
     
-    #Randomly data generation based on the setting on datagen.jl
-    srand(b * 100)
-    m = MixtureModel(map((u, v) -> Normal(u, v), mu_true, sigmas_true), wi_true)
-    gamma_true = rand(m, nF)
-    
-    prob = exp(gamma_true[groupindex] .+ X*betas_true)
-    prob= prob ./ (1 .+ prob)
-    Y = Bool[rand(Binomial(1, prob[i])) == 1 for i in 1:N];
-    X = X .- mean(X, 1);
-
     lr=LatentGaussianMixtureModel.EMtest(X, Y, groupindex, Calternative-1, ntrials=ntrials, debuginfo=debuginfo, ctauparallel=false, ngh=100)
     println("Mission $b completed with lr=$lr")
     lr
--- examples/datagen.jl
+++ examples/datagen.jl
@@ -1,37 +1,33 @@
+import Distributions, StatsBase
+@everywhere function datagen(n::Int, Ctrue::Int, N::Int, J::Int, n_ij::Vector{Int64}; b::Int=round(Int,rand()*1000))
 
-using Distributions, StatsBase
+    srand(200*b)
+    groupindex = StatsBase.inverse_rle(1:n, n_ij)    
+    X = Distributions.rand(Distributions.TDist(8), N, 2) .* [14.71 4.47] ./ std(Distributions.TDist(8))
+    betas_true=[-0.0274, -0.00878]
+    mu_all = -log(1/0.81 - 1)
+    if Ctrue == 1
+        mu_true = [mu_all]
+        wi_true = [1.0]
+        sigmas_true = [0.3493]
+    elseif Ctrue == 2
+        mu_true = [mu_all - 1.0, mu_all + 1.2]
+        wi_true = [.3, .7]
+        sigmas_true = [.6, .5]
+    elseif Ctrue == 3
+        mu_true = [mu_all - 1.0, mu_all, mu_all + 2.;]
+        wi_true = [.2, .5, .3]
+        sigmas_true = [.6, .4, .6]
+    end
 
+    #Randomly data generation based on the setting on datagen.jl
+    srand(b * 100)
+    m = Distributions.MixtureModel(map((u, v) -> Distributions.Normal(u, v), mu_true, sigmas_true), wi_true)
+    gamma_true = rand(m, n)
 
-n = 282
-srand(100)
-n_ij = round(Int64, rand(Poisson(55), 282).+rand(Exponential(95), 282))
-N = sum(n_ij)
-
-groupindex = inverse_rle(1:n, n_ij)
-J=2  #beta dimension
-srand(200*b)
-X = rand(TDist(8), N, 2) .* [14.71 .447] ./ std(TDist(8))
-betas_true=[0.0274, 0.0878]
-if Ctrue == 1
-    mu_true = [-1.54]
-    wi_true = [1.0]
-    sigmas_true = [0.3493]
-elseif Ctrue == 2
-    mu_true = [-2.0858,-1.4879]
-    wi_true = [0.0828,0.9172]
-    sigmas_true = [0.6735,0.2931]
-elseif Ctrue == 3
-    mu_true = [log(1/0.779 - 1) - 4.0, log(1/0.779 - 1) + 1.0, log(1/0.779 - 1) + 4.0;]
-    wi_true = [.3, .4, .3]
-    sigmas_true = [1.2, .8, .9]
+    prob = exp(gamma_true[groupindex] .+ X*betas_true)
+    prob= prob ./ (1 .+ prob)
+    Y = Bool[Distributions.rand(Distributions.Binomial(1, prob[i])) == 1 for i in 1:N];
+    X = X .- mean(X, 1);
+    return(X, Y, groupindex, wi_true, mu_true, sigmas_true, m, gamma_true)
 end
-
-#Randomly data generation based on the setting on datagen.jl
-srand(b * 100)
-m = MixtureModel(map((u, v) -> Normal(u, v), mu_true, sigmas_true), wi_true)
-gamma_true = rand(m, n)
-
-prob = exp(gamma_true[groupindex] .+ X*betas_true)
-prob= prob ./ (1 .+ prob)
-Y = Bool[rand(Binomial(1, prob[i])) == 1 for i in 1:N];
-X = X .- mean(X, 1);
