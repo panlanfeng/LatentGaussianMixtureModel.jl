8a0e2f6586ffdcbbe84201b08b331adc21f145d4
Lanfeng Pan <panlanfeng@gmail.com>
2016-02-29 20:51:50 -0600

    choose between two true parameters

--- examples/datagen.jl
+++ examples/datagen.jl
@@ -1,23 +1,40 @@
 import Distributions, StatsBase
-@everywhere function datagen(n::Int, Ctrue::Int, N::Int, J::Int, n_ij::Vector{Int64}; b::Int=round(Int,rand()*1000))
+@everywhere function datagen(n::Int, Ctrue::Int, N::Int, J::Int, n_ij::Vector{Int64}; b::Int=round(Int,rand()*1000), adjustmean::Bool=false)
 
     srand(200*b)
     groupindex = StatsBase.inverse_rle(1:n, n_ij)    
-    X = Distributions.rand(Distributions.TDist(8), N, 2) .* [14.71 4.47] ./ std(Distributions.TDist(8))
-    betas_true=[-0.0274, -0.00878]
-    mu_all = -log(1/0.81 - 1)
-    if Ctrue == 1
-        mu_true = [mu_all]
-        wi_true = [1.0]
-        sigmas_true = [0.3493]
-    elseif Ctrue == 2
-        mu_true = [mu_all - 1.0, mu_all + 1.2]
-        wi_true = [.3, .7]
-        sigmas_true = [.6, .5]
-    elseif Ctrue == 3
-        mu_true = [mu_all - 1.0, mu_all, mu_all + 2.;]
-        wi_true = [.2, .5, .3]
-        sigmas_true = [.6, .4, .6]
+    X = Distributions.rand(Distributions.Normal(), N, J) 
+    betas_true=ones(J)
+    if adjustmean
+        mu_all = -log(1/0.81 - 1)
+        if Ctrue == 1
+            mu_true = [mu_all]
+            wi_true = [1.0]
+            sigmas_true = [0.3493]
+        elseif Ctrue == 2
+            mu_true = [mu_all - 1.0, mu_all + 1.2]
+            wi_true = [.3, .7]
+            sigmas_true = [.6, .5]
+        elseif Ctrue == 3
+            mu_true = [mu_all - 1.0, mu_all, mu_all + 2.;]
+            wi_true = [.2, .5, .3]
+            sigmas_true = [.6, .4, .6]
+        end
+    else
+        mu_all = log(1/0.779 - 1)
+        if Ctrue == 1
+            mu_true = [mu_all]
+            wi_true = [1.0]
+            sigmas_true = [1.2]
+        elseif Ctrue == 2
+            mu_true = [mu_all/2 - 2.0, mu_all/2 + 2.0] 
+            wi_true =  [.5, .5]
+            sigmas_true = [1.2, .8]
+        elseif Ctrue == 3
+            mu_true = [mu_all/3 - 4.0, mu_all/3 + 1.0, mu_all/3 + 4.0;]
+            wi_true = [.3, .4, .3]
+            sigmas_true = [1.2, .8, .9]
+        end
     end
 
     #Randomly data generation based on the setting on datagen.jl
@@ -29,5 +46,5 @@ import Distributions, StatsBase
     prob= prob ./ (1 .+ prob)
     Y = Bool[Distributions.rand(Distributions.Binomial(1, prob[i])) == 1 for i in 1:N];
     X = X .- mean(X, 1);
-    return(X, Y, groupindex, wi_true, mu_true, sigmas_true, m, gamma_true)
+    return(X, Y, groupindex, wi_true, mu_true, sigmas_true, betas_true, m, gamma_true)
 end
