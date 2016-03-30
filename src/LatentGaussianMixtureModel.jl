7c46fe7bc6ca63576ab021537c9c18cc8c325750
Lanfeng Pan <panlanfeng@gmail.com>
2016-03-29 19:33:07 -0500

    add formula method and imputeY

--- src/LatentGaussianMixtureModel.jl
+++ src/LatentGaussianMixtureModel.jl
@@ -10,14 +10,17 @@ import GaussianMixtureTest: gmm, pn, decidepenalty
 import StatsBase: RealArray, RealVector, RealMatrix, IntegerArray, IntegerVector, IntegerMatrix, IntUnitRange
 import StatsFuns: logπ
 export predictgamma, asymptoticdistribution, latentgmm, marginallikelihood, EMtest, FDR, coefpvalue, vcov
-include("arithmetic.jl")
-include("utility.jl")
 
 import StatsBase: coef, coeftable, confint, deviance, loglikelihood, nobs, stderr, vcov, residuals, predict, fit, model_response
 export coef, coeftable, confint, deviance, loglikelihood, nobs, stderr, vcov, residuals, predict, fit, fit!, model_response
+using DataFrames
+
 include("model.jl")
-export LGMModel
+export LGMModel, imputeY!
 #include("MCEM.jl")
+
+include("arithmetic.jl")
+include("utility.jl")
 include("EM.jl")
 #include("FractionalImputation.jl")
 
--- src/model.jl
+++ src/model.jl
@@ -36,7 +36,11 @@ type LGMModel <: RegressionModel
     tau::Real
     fit::Bool
 
-    function LGMModel(X::Matrix{Float64}, Y::Vector{Bool}, groupindex::Vector{Int}, ncomponent::Int, ngh::Int=100)
+    function LGMModel(X::Matrix{Float64}, Y::Vector{Bool}, groupindex::Vector{Int}, ncomponent::Int;
+        ngh::Int=100,
+        taufixed::Bool=false, whichtosplit=1, tau=.5,
+        sn=ones(ncomponent), an=1.0/maximum(groupindex))
+        
         n = maximum(groupindex)
         N, J = size(X)
         Wim::Matrix{Float64}=zeros(n, ncomponent*ngh)
@@ -55,16 +59,11 @@ type LGMModel <: RegressionModel
         μ = sort!(randn(ncomponent))
         σ = ones(ncomponent)
         β = randn(J)
+        ll = -Inf
         μ_lb=fill(-Inf, ncomponent)
         μ_ub=fill(Inf, ncomponent)
         σ_lb = .25 .* σ
         σ_ub = 2.0 .* σ
-        ll = 0.0
-        sn=ones(ncomponent)
-        an=1.0/n
-        taufixed=false
-        whichtosplit=1
-        tau=.5
         
         new(X, Y, groupindex, ncomponent, p, μ, σ, β, n, ngh, ghx, ghw, μ_lb, μ_ub, σ_lb, σ_ub, Wim, Wm, lln, llN, llN2, llN3, Xscratch, xb, gammaM, XWX, XWY, ll, sn, an, taufixed, whichtosplit, tau, false)
     end
@@ -247,7 +246,7 @@ function loglikelihood(m::LGMModel)
     m.ll
 end
 
-function vcov(m::LGMModel; debuginfo::Bool=false)
+function infomatrix(m::LGMModel; debuginfo::Bool=false, includelambda::Bool=true)
 
     N,J = size(m.X)
     n = m.n
@@ -259,8 +258,11 @@ function vcov(m::LGMModel; debuginfo::Bool=false)
     S_β = zeros(n, J)
     S_π = zeros(n, C-1)
     S_μσ = zeros(n, 2*C)
-    A_mul_B!(m.xb, m.X, m.β)
+    if includelambda
+        S_λ = zeros(n, 2*C)
+    end 
     ml = zeros(n)
+    A_mul_B!(m.xb, m.X, m.β)
     for jcom in 1:C
         for ix in 1:ngh
             ixM = ix+ngh*(jcom-1)
@@ -314,6 +316,10 @@ function vcov(m::LGMModel; debuginfo::Bool=false)
             ind = (1+ngh*(kcom-1)):ngh*kcom
             S_μσ[i, 2*kcom-1] = sumexp(m.Wim[i, ind], H1(m.gammaM[ind], m.μ[kcom], m.σ[kcom])) / ml[i]
             S_μσ[i, 2*kcom] = sumexp(m.Wim[i, ind], H2(m.gammaM[ind], m.μ[kcom], m.σ[kcom]))/ml[i]
+            if includelambda
+                S_λ[i, 2*kcom-1] = sumexp(m.Wim[i, ind], H3(m.gammaM[ind], m.μ[kcom], m.σ[kcom]))/ml[i]
+                S_λ[i, 2*kcom] = sumexp(m.Wim[i, ind], H4(m.gammaM[ind], m.μ[kcom], m.σ[kcom]))/ml[i]
+            end
         end
         for j in 1:J
             S_β[i, j] = sumexp(m.Wim[i,:], summat_beta[i, :, j])/ml[i]
@@ -323,29 +329,123 @@ function vcov(m::LGMModel; debuginfo::Bool=false)
     debuginfo && println(round(S_η[1:5,:], 5))
     debuginfo && println(round(sum(S_η, 1)./sqrt(n), 6))
     I_η = S_η'*S_η./n
-    if 1/cond(I_η) < eps(Float64)
+    if includelambda
+        I_η = S_η'*S_η./n
+        I_λη = S_λ'*S_η./n
+        I_λ = S_λ'*S_λ./n
+        I_all = vcat(hcat(I_η, I_λη'), hcat(I_λη, I_λ))
+    else
+        I_all = I_η
+    end
+    if 1/cond(I_all) < eps(Float64)
         warn("Information Matrix is singular!")
-        D, V = eig(I_η)
+        D, V = eig(I_all)
         debuginfo && println(D)
         tol2 = maximum(abs(D)) * 1e-14
         D[D.<tol2] = tol2
-        I_η = V*diagm(D)*V'
+        I_all = V*diagm(D)*V'
     end
-    inv(I_η)[1:J,1:J]./n
+    return I_all
+    #return inv(I_all)[1:J,1:J]./n
 end
 
-function predict(m::LGMModel)
+function vcov(m::LGMModel; debuginfo::Bool=false)
+    J = size(m.X, 2)
+    I_all = infomatrix(m, debuginfo=debuginfo)
+    return inv(I_all)[1:J, 1:J] ./ m.n
+end
+
+function asymptoticdistribution(m::LGMModel; debuginfo::Bool=false, nrep::Int=10000)
+    N,J = size(m.X)
+    n = m.n
+    ngh=m.ngh
+    C = m.ncomponent
+    if C == 1
+        return rand(Chisq(2), nrep)
+    end
+    
+    I_all = infomatrix(m, includelambda=true, debuginfo=debuginfo)
+    I_λ_η = I_all[(J+3*C):(J+5*C-1), (J+3*C):(J+5*C-1)] - I_all[(J+3*C):(J+5*C-1), 1:(J+3*C-1)] * inv(I_all[1:(J+3*C-1), 1:(J+3*C-1)]) * I_all[1:(J+3*C-1),(J+3*C):(J+5*C-1) ]
+    debuginfo && println(round(I_λ_η, 6))
+    D, V = eig(I_λ_η)
+    D[D.<0.] = 0.
+    I_λ_η2 = V * diagm(sqrt(D)) * V'
+    u = randn(nrep, 2*C) * I_λ_η2
+    EM = zeros(nrep, C)
+    T = zeros(nrep)
+    for kcom in 1:C
+        EM[:, kcom] = sum(u[:, (2*kcom-1):(2*kcom)] * inv(I_λ_η[(2*kcom-1):(2*kcom), (2*kcom-1):(2*kcom)]) .* u[:, (2*kcom-1):(2*kcom)], 2)
+    end
+    for i in 1:nrep
+        T[i] = maximum(EM[i, :])
+    end
+    T
+end
+
+function predict(m::LGMModel, newX::Matrix{Float64}, newgroup::Vector{Int})
     !m.fit && warn("The model is not fitted!")
     N, J = size(m.X)
-    A_mul_B!(m.xb, m.X, m.β)
+    N2, J2 = size(newX)
+    J != J2 && error("Dimension Dismatch in new data")
+    newxb = zeros(N2)
+    newgamma = zeros(N2)
+    A_mul_B!(newxb, newX, m.β)
     gammaprediction = ranef(m)
-    for i in 1:N
-        m.llN[i]=m.xb[i] + gammaprediction[m.groupindex[i]]
+    overallmean = sum(m.μ .* m.p)
+    for i in eachindex(newgroup)
+        if newgroup[i] > m.n
+            newgamma[i] = overallmean
+        else
+            newgamma[i] = gammaprediction[newgroup[i]]
+        end
     end
-    m.llN
+    for i in 1:N2
+        newxb[i] += newgamma[i]
+    end
+    logistic!(newxb)
 end
 
-function imputeY(m::LGMModel)
+function latentgmm(f::Formula, fr::AbstractDataFrame, ncomponent::Int; kwargs...)
+    mf = ModelFrame(f,fr)
+    X = ModelMatrix(mf).m
+    Y = convert(Vector{Bool}, getindex(fr, f.lhs))
+    retrms = filter(x->Meta.isexpr(x,:call) && x.args[1] == :|, mf.terms.terms)
+    if length(retrms) ≤ 0
+        throw(ArgumentError("$f has no random-effects terms"))
+    end
+    # groupindex =  sort!([remat(e,mf.df) for e in retrms]; by = nlevs, rev = true) #or remat(retrms[1], mf.df).f
+    groupindex = convert(Vector{Int}, getindex(mf.df, retrms[1].args[3]))
+    m = LGMModel(X, Y, groupindex, ncomponent; kwargs...)
+    return m
+end
+
+function imputeY!(m::LGMModel, m0::LGMModel, naindicator::Vector{Bool}, nMI::Int=10; kwargs...)
     
+    # naindicator = isnan(m.Y)
+    missingμ = predict(m0, m.X[naindicator,:], m.groupindex[naindicator])
+    m.p = m0.p
+    m.μ = m0.μ
+    m.σ = m0.σ
+    m.β = m0.β
+    pm = repmat(m0.p, 1, nMI)
+    μm = repmat(m0.μ, 1, nMI)
+    σm = repmat(m0.σ, 1, nMI)
+    βm = repmat(m0.β, 1, nMI)
+    ml = -Inf .* ones(nMI)
     
+    for im in 1:nMI
+        m.Y[naindicator]=Bool[rand(Binomial(1, missingμ[i])) == 1 for i in eachindex(missingμ)];
+        fit!(m; kwargs...)
+        pm[:, im] = m.p
+        μm[:, im] = m.μ
+        σm[:, im] = m.σ
+        βm[:, im] = m.β
+        ml[im] = m.ll
+    end    
+    m.p = mean(pm, 2)[:]
+    m.μ = mean(μm, 2)[:]
+    m.σ = mean(σm, 2)[:]
+    m.β = mean(βm, 2)[:]
+    m.ll = mean(ml)
+    m
 end
