3e58a796dbccca1732ec4d0bb7958135abc25b80
Lanfeng Pan <panlanfeng@gmail.com>
2016-07-25 21:08:31 -0500

    challenge power test

---
+++ challenge/runchallenge.jl
@@ -0,0 +1,15 @@
+#julia4 --machinefile=$PBS_NODEFILE -- run100.jl 1 2 110
+#run on all available cores using:
+addprocs(readcsv(ENV["PBS_NODEFILE"], ASCIIString)[:, 1])
+@everywhere args = @fetchfrom 1 ARGS
+
+include(joinpath(Pkg.dir("LatentGaussianMixtureModel"), "challenge/challengesim.jl"))
+Xraw=readcsv(joinpath(Pkg.dir("LatentGaussianMixtureModel"), "challenge/X.csv"))
+groupindex_raw=readcsv(joinpath(Pkg.dir("LatentGaussianMixtureModel"), "challenge/groupindex.csv"), Int)[:,1]
+@everywhere X = @fetchfrom 1 Xraw
+@everywhere groupindex = @fetchfrom 1 groupindex_raw
+
+teststat = pmap((b) -> Brun(b, X, groupindex), 1:parse(Int, args[1]))
+writecsv("teststat$(Ctrue)$(Calternative).csv", teststat)
+run(`sed -i "s/(//g" teststat$(Ctrue)$(Calternative).csv`)
+run(`sed -i "s/)//g" teststat$(Ctrue)$(Calternative).csv`)
