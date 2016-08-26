391244f7a670be6efc8127a6d25a8a43ab0f3423
Lanfeng Pan <panlanfeng@gmail.com>
2016-08-26 11:47:19 -0500

    bicrun200

---
+++ biccompare/bicrun200.jl
@@ -0,0 +1,11 @@
+#julia4 --machinefile=$PBS_NODEFILE -- run100.jl 1 2 110
+#run on all available cores using:
+#addprocs(readcsv(ENV["PBS_NODEFILE"], ASCIIString)[:, 1])
+@everywhere args = @fetchfrom 1 ARGS
+
+include(joinpath(Pkg.dir("LatentGaussianMixtureModel"), "biccompare/runbic.jl"))
+@everywhere C=parse(Int,args[2])
+teststat = pmap((b) -> Brun(b, C), 1:parse(Int, args[1]))
+writecsv("bic$(C).csv", teststat)
+run(`sed -i "s/(//g" bic$(C).csv`)
+run(`sed -i "s/)//g" bic$(C).csv`)
