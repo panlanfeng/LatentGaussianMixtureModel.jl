bdd5bb6545fabc385216c6b71f2371dbe7ae3719
Lanfeng Pan <panlanfeng@gmail.com>
2016-09-06 22:20:19 -0500

    fix sequential test erro and change result file name

--- teststat200/run200.jl
+++ teststat200/run200.jl
@@ -3,10 +3,10 @@
 addprocs(readcsv(ENV["PBS_NODEFILE"], ASCIIString)[:, 1])
 @everywhere args = @fetchfrom 1 ARGS
 
-include(joinpath(Pkg.dir("LatentGaussianMixtureModel"), "examples/TestAsymptoticDistribution.jl"))
+include(joinpath(Pkg.dir("LatentGaussianMixtureModel"), "teststat200/TestAsymptoticDistribution.jl"))
 @everywhere Ctrue = parse(Int, args[1])
 @everywhere Calternative = parse(Int, args[2])
 teststat = pmap((b) -> Brun(b, Ctrue, Calternative, showpower=true), 1:parse(Int, args[3]))
-writecsv("teststat$(Ctrue)$(Calternative).csv", teststat)
-run(`sed -i "s/(//g" teststat$(Ctrue)$(Calternative).csv`)
-run(`sed -i "s/)//g" teststat$(Ctrue)$(Calternative).csv`)
+writecsv("seq$(Ctrue)$(Calternative).csv", teststat)
+run(`sed -i "s/(//g" seq$(Ctrue)$(Calternative).csv`)
+run(`sed -i "s/)//g" seq$(Ctrue)$(Calternative).csv`)
