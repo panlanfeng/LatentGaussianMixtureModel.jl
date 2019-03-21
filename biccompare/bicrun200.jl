884234268f80b3aa651b8ef484142b7e661b7506
Lanfeng Pan <planfeng@amazon.com>
2019-03-21 09:55:00 -0700

    more change in bic simulation in 1.0

--- biccompare/bicrun200.jl
+++ biccompare/bicrun200.jl
@@ -2,8 +2,9 @@
 #run on all available cores using:
 #addprocs(readcsv(ENV["PBS_NODEFILE"], ASCIIString)[:, 1])
 @everywhere args = @fetchfrom 1 ARGS
-
-include(joinpath(Pkg.dir("LatentGaussianMixtureModel"), "biccompare/runbic.jl"))
+import LatentGaussianMixtureModel
+#include(joinpath(Pkg.dir("LatentGaussianMixtureModel"), "biccompare/runbic.jl"))
+include(joinpath(dirname(pathof(LatentGaussianMixtureModel)), "..", "biccompare/runbic.jl"))
 @everywhere C=parse(Int,args[2])
 teststat = pmap((b) -> Brun(b, C), 1:parse(Int, args[1]))
 writecsv("bic$(C).csv", teststat)
--- biccompare/runbic.jl
+++ biccompare/runbic.jl
@@ -28,7 +28,7 @@ import Yeppp
         wi_true =  [.6, .4]
         sigmas_true = [1.2, .8]
     elseif Ctrue == 3
-        mu_true = [mu_all - 2.0, mu_all + 1.0, mu_all + 3.5;]
+        mu_true = [mu_all - 2.0, mu_all + 1.0, mu_all + 3.5]
         wi_true = [.3, .4, .3]
         sigmas_true = [1.2, .8, .9]
     end
@@ -38,16 +38,16 @@ import Yeppp
     m = MixtureModel(map((u, v) -> Normal(u, v), mu_true, sigmas_true), wi_true)
     gamma_true = rand(m, nF)
 
-    prob = exp(gamma_true[groupindex] .+ X*betas_true)
+    prob = exp.(gamma_true[groupindex] .+ X*betas_true)
     prob= prob ./ (1 .+ prob)
     Y = Bool[rand(Binomial(1, prob[i])) == 1 for i in 1:N];
-    X = X .- mean(X, 1);
+    X = X .- mean(X, dims=1);
 
     vb=fill(-Inf, ncomponent)
     for kcom in 1:ncomponent
         # res = LatentGaussianMixtureModel.latentgmm(X, Y, groupindex, kcom, maxiteration=1000, tol=0.001)
-        res = LatentGaussianMixtureModel.latentgmmrepeat(X, Y, groupindex, kcom, ntrials=5)
-        vb[kcom]=res[5]-kcom*log(nF)*Cn/2.0
+        res = LatentGaussianMixtureModel.latentgmmrepeat(X, Y, groupindex, kcom, ntrials=25)
+        vb[kcom]=res[5]#-kcom*log(nF)*Cn/2.0
     end
     bic,C=findmax(vb)
     println("Mission $b chooses $C components.")
