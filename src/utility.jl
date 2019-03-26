f7bdcfadd9252410cfd5e93bf2926c726a671fca
Lanfeng Pan <planfeng@amazon.com>
2019-03-25 21:52:31 -0700

    fix typo

--- src/utility.jl
+++ src/utility.jl
@@ -239,14 +239,14 @@ function asymptoticdistribution(X::Matrix{Float64}, Y::AbstractArray{Bool, 1}, g
         debuginfo && println(D)
         tol2 = maximum(abs.(D)) * 1e-14
         D[D.<tol2] .= tol2
-        I_all = V*Matri(Diagonal(D))*V'
+        I_all = V*Diagonal(D)*V'
     end
     debuginfo && println(round.(I_all, 6))
     I_λ_η = I_all[(J+3*C):(J+5*C-1), (J+3*C):(J+5*C-1)] - I_all[(J+3*C):(J+5*C-1), 1:(J+3*C-1)] * inv(I_all[1:(J+3*C-1), 1:(J+3*C-1)]) * I_all[1:(J+3*C-1),(J+3*C):(J+5*C-1) ]
     debuginfo && println(round.(I_λ_η, 6))
     D, V = eigen(I_λ_η)
     D[D.<0.] .= 0.
-    I_λ_η2 = V * Matrix(Diagonal(sqrt.(D))) * V'
+    I_λ_η2 = V * Diagonal(sqrt.(D)) * V'
     u = randn(nrep, 2*C) * I_λ_η2
     EM = zeros(nrep, C)
     T = zeros(nrep)
